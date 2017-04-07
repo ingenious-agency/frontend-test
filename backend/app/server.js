055df5ab50d974bd563d8ffa73f03f48590b66b5
David Hernández Bahr <tulkas.eldarion@gmail.com>
2017-04-07 16:49:23 -0400

    Adding cors support to backend.

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,29 +1,39 @@
 var db = require('./db');
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/events', function (req, res) {
-  res.json({events: db.findAll()});
+  res.json({
+    events: db.findAll()
+  });
 });
 
 app.get('/events/featured', function (req, res) {
-  res.json({events: db.featured()});
+  res.json({
+    events: db.featured()
+  });
 });
 
 app.get('/events/:id', function (req, res) {
   var event = db.finById(req.params.id);
-  if(event) {
-    res.json({event: event});
+  if (event) {
+    res.json({
+      event: event
+    });
   } else {
     res.sendStatus(404);
   }
 });
 
 app.post('/events', function (req, res) {
-  res.json({event: db.create(req.body.event)});
+  res.json({
+    event: db.create(req.body.event)
+  });
 });
 
 module.exports = app;
